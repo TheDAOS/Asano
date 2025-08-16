83eb2e87bb8bdf189dfad58ac8517b8e1961dbfc
Sanju Santhosh <68913047+TheDAOS@users.noreply.github.com>
2025-08-16 22:15:03 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="bg-gradient-to-br bg-fixed from-black via-gray-900 to-gray-800 text-white">
+      <body className="bg-gradient-to-br bg-fixed from-black via-gray-900 to-gray-800 text-white overflow-y-auto">
         <StoreProvider>
           {children}
         </StoreProvider>
