5ab9ca5376c2e3082fea383e3c790240f7b57682
TheDAOS <sanjusanthosh46@gmail.com>
2025-11-16 02:25:41 +0530

    feat: Update layout and styling for Asano app, add manifest and icons

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,20 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div className="flex justify-center items-center w-screen h-screen bg-linear-150 from-lime-300/60 via-lime-300/30 to-lime-300/10">
+      <div className="max-w-[90%] mx-auto px-4 py-3 bg-black/30 backdrop-blur-md rounded-2xl border border-white/15 shadow-lg flex flex-col gap-6">
+        <div className="w-fit flex flex-col gap-1">
+          <div className="w-fit text-2xl font-extrabold px-3 py-1 backdrop-blur-md rounded-full border border-white/15 shadow-lg">
+            <h1>Asano</h1>
+          </div>
+          <div className="w-fit text-lg px-3 py-1 backdrop-blur-md rounded-full border border-white/15 shadow-lg">
+            <span>Personal Finance App</span>
+          </div>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+
+        <div className="px-4 py-3 backdrop-blur-md rounded-2xl border border-white/15 shadow-lg">
+          <span>A modern personal finance tracking application built with Next.js 16</span>
         </div>
-      </main>
+      </div>
     </div>
   );
 }
